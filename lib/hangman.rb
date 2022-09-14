c3583295dcaac2ef2f0138ff97dee37e4d66d790
kckuei <kevinkuei@gmail.com>
2022-09-13 17:08:31 -0700

    Add class scaffolding, implement dictionary class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -59,7 +59,38 @@ HANGMANPICS = ["
       |
 ========="]
 
-def display
-  puts LOGO
+# Hangman class
+class Hangman; end
+
+# Dictionary class
+class Dictionary
+  def initialize(dict_path, seed = 1234)
+    file = File.open(dict_path, 'r')
+    @words = file.readlines.map(&:chomp)
+    file.close
+    @random = Random.new(seed)
+  end
+
+  def random_word
+    random_num = @random.rand(@words.length)
+    @words[random_num]
+  end
 end
-display
+
+# Serializer class
+class Serializer; end
+
+# Display class
+class Display; end
+
+# Player class
+class Player; end
+
+# Computer class
+class Computer < Player; end
+
+# Human class
+class Human < Player; end
+
+dict = Dictionary.new('./../dictionary/google-10000-english-no-swears.txt', 1337)
+puts dict.random_word
